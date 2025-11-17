15d201e4131a3abc15e6f9691cbd3a0d6a1c49c3
asharykin <aleksandr-sharykin@mail.ru>
2025-11-17 23:55:35 +0300

    04_02

---
+++ src/04_02.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+template<typename T>
+T max(T val) {
+    return val;
+}
+
+template<typename T, typename ... Ts>
+T max(T first, Ts... rest) {
+    return first > max(rest...) ? first : max(rest...);
+}
+
+template<typename T>
+T min(T val) {
+    return val;
+}
+
+template<typename T, typename ... Ts>
+T min(T first, Ts ... rest) {
+    return first < min(rest...) ? first : min(rest...);
+}
+
+template<typename ... Ts>
+double sum(Ts... rest) {
+    return (rest + ...);
+}
+
+template<typename ... Ts>
+double average(Ts ... rest) {
+    return sum(rest...) / sizeof...(rest);
+}
+
+int main() {
+    std::cout << "max: " << max(3.5, 7.2, 1.5, 4.8) << std::endl;
+    std::cout << "min: " << min(3.5, 7.2, 1.5, 4.8) << std::endl;
+    std::cout << "sum: " << sum(3.5, 7.2, 1.5, 4.8) << std::endl;
+    std::cout << "average: " << average(3.5, 7.2, 1.5, 4.8) << std::endl;
+}
