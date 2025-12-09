2f8b3fa26f4cb0495aa711112c0de3b5d17d572d
asharykin <aleksandr-sharykin@mail.ru>
2025-12-09 23:13:26 +0300

    06_01

---
+++ src/6/06_01/rational.cpp
@@ -0,0 +1,70 @@
+#include "rational.h"
+#include <numeric>
+
+void Rational::reduce()
+{
+    auto gcd = std::gcd(m_num, m_den);
+    m_num /= gcd;
+    m_den /= gcd;
+}
+
+Rational& Rational::operator+=(Rational const& other)
+{
+    int lcm_den = std::lcm(m_den, other.m_den);
+    m_num = m_num * (lcm_den / m_den) + other.m_num * (lcm_den / other.m_den);
+    m_den = lcm_den;
+    reduce();
+    return *this;
+}
+
+Rational& Rational::operator-=(Rational const& other)
+{
+    return *this += Rational(-other.m_num, other.m_den);
+}
+
+Rational& Rational::operator*=(Rational const& other)
+{
+    m_num *= other.m_num;
+    m_den *= other.m_den;
+    reduce();
+    return *this;
+}
+
+Rational& Rational::operator/=(Rational const& other)
+{
+    return *this *= Rational(other.m_den, other.m_num);
+}
+
+Rational Rational::operator++(int)
+{
+    Rational copy = *this;
+    ++(*this);
+    return copy;
+}
+
+Rational Rational::operator--(int)
+{
+    Rational copy = *this;
+    --(*this);
+    return copy;
+}
+
+std::strong_ordering operator<=>(Rational const& lhs, Rational const& rhs)
+{
+    return lhs.m_num * rhs.m_den <=> rhs.m_num * lhs.m_den;
+}
+
+bool operator==(Rational const& lhs, Rational const& rhs)
+{
+    return (lhs <=> rhs) == std::strong_ordering::equal;
+}
+
+std::istream& operator>>(std::istream& stream, Rational& rational)
+{
+    return (stream >> rational.m_num).ignore() >> rational.m_den;
+}
+
+std::ostream& operator<<(std::ostream& stream, Rational const& rational)
+{
+    return stream << rational.m_num << '/' << rational.m_den;
+}
