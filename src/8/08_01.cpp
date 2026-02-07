2a7d40bc95a300095eec7018abb79088f7019978
asharykin <aleksandr-sharykin@mail.ru>
2026-02-07 23:56:26 +0300

    08_01

--- src/8/08_01.cpp
+++ src/8/08_01.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+// Класс с закрытыми данными
+class Entity_v1
+{
+public:
+    Entity_v1(int value) : privateValue(value) { }
+
+    void printValue() const
+    {
+        std::cout << "Entity_v1.privateValue = " << privateValue << std::endl;
+    }
+
+private:
+    int privateValue;
+};
+
+class Entity_v2
+{
+public:
+    int publicValue;
+};
+
+int main()
+{
+    Entity_v1 e1(42);
+    e1.printValue();
+
+    reinterpret_cast<Entity_v2&>(e1).publicValue = 100;
+
+    e1.printValue();
+}
