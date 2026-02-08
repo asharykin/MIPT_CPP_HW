3b9c4d87cc2e2e7b77ef33f546b92bf485489e09
asharykin <aleksandr-sharykin@mail.ru>
2026-02-08 18:49:32 +0300

    08_01

--- src/8/08_01.cpp
+++ src/8/08_01.cpp
@@ -1,32 +1,44 @@
+#include <cassert>
 #include <iostream>
 
-// Класс с закрытыми данными
 class Entity_v1
 {
 public:
-    Entity_v1(int value) : privateValue(value) { }
+    Entity_v1(int value) : m_value_private(value) { }
 
-    void printValue() const
+    int get_value() const
     {
-        std::cout << "Entity_v1.privateValue = " << privateValue << std::endl;
+        return m_value_private;
     }
 
 private:
-    int privateValue;
+    int m_value_private;
 };
 
 class Entity_v2
 {
 public:
-    int publicValue;
+    int m_value_public;
+};
+
+union Entity_Union {
+    Entity_v1* p_e1;
+    Entity_v2* p_e2;
+
+    Entity_Union(Entity_v1* target) {
+        p_e1 = target;
+    }
 };
 
 int main()
 {
-    Entity_v1 e1(42);
-    e1.printValue();
+    Entity_v1 entity(50);
+    assert(entity.get_value() == 50);
 
-    reinterpret_cast<Entity_v2&>(e1).publicValue = 100;
+    reinterpret_cast<Entity_v2&>(entity).m_value_public = 100;
+    assert(entity.get_value() == 100);
 
-    e1.printValue();
+    Entity_Union entity_union(&entity);
+    entity_union.p_e2->m_value_public = 200;
+    assert(entity.get_value() == 200);
 }
