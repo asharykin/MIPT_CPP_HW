14bf008ec30a0ca4b7921ac7897173090f8d1fa8
asharykin <aleksandr-sharykin@mail.ru>
2026-02-11 22:34:05 +0300

    RIP assert(), you will be missed

--- src/8/08_01.cpp
+++ src/8/08_01.cpp
@@ -29,27 +29,32 @@ union Entity_Union {
     }
 };
 
-class EntityTest : public ::testing::Test {
+class EntityTest : public ::testing::Test
+{
 protected:
     Entity_v1 entity{50};
 };
 
-TEST_F(EntityTest, InitialValueIsCorrect) {
+TEST_F(EntityTest, InitialValueIsCorrect)
+{
     EXPECT_EQ(entity.get_value(), 50);
 }
 
-TEST_F(EntityTest, ModifyViaReinterpretCast) {
+TEST_F(EntityTest, ModifyViaReinterpretCast)
+{
     reinterpret_cast<Entity_v2&>(entity).m_value_public = 100;
     EXPECT_EQ(entity.get_value(), 100);
 }
 
-TEST_F(EntityTest, ModifyViaUnion) {
+TEST_F(EntityTest, ModifyViaUnion)
+{
     Entity_Union entity_union(&entity);
     entity_union.p_e2->m_value_public = 200;
     EXPECT_EQ(entity.get_value(), 200);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
