4fde8861a70642ae1593016a18460f753aee4c22
Александр <aleksandr-sharykin@mail.ru>
2026-08-30 23:24:42 +0300

    07_01

--- src/07/07_01.cpp
+++ src/07/07_01.cpp
@@ -56,6 +56,7 @@ int main()
     
     // 0*x^2 + 0*x + 2 = 0; no roots
     assert(!solve(0.0, 0.0, 2.0).has_value());
+    
     // 0*x^2 + 1*x - 2 = 0; x = 2
     assert(equals( std::get<double>( solve(0.0, 1.0, -2.0).value() ), 2.0 ));
     // 1*x^2 - 2*x + 3 = 0; no roots
